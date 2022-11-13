1411808882ce6a7f4fcf85bd2173738fa0bd529a
Mohamed Osoble <mohamedosoble@outlook.com>
2022-11-13 15:16:04 +0000

    add pseudocode for the game pseudocode added in the scripts.js file

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
-console.log("Hello world");
+function getComputerChoice() {
+
+    // 1. obtain random choice from rock paper scissors
+
+    // 2. return random choice as output from function
+};
+
+function playRound(playerChoice, computerChoice) {
+    // 1. Compare player choice to computer choice
+
+    // 2. If player choice is same as computer choice, return 'draw'
+
+    // 3. If player choice is rock and computer chooses scissors, return 'you win'
+
+    // 4. If player choice is paper and computer choice is rock, return 'you win'
+
+    // 5. If player choice is scissors and computer choice is rock, return 'you win'
+
+    // 6. Return computer wins in all other scenarios
+};
+
+function game() {
+    // set up for loop to play 5 rounds
+        // obtain user input
+        // obtain computer input
+        // call playround with both inputs
+        // update score
+
+    // once loop is complete, return rounds won, lost and drawn.
+};
