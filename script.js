e2112dc9e1500e8b5a6d0e0b7f7ca6697542bdf3
Mohamed Osoble <mohamedosoble@outlook.com>
2022-11-13 16:34:24 +0000

    complete getComputerChoice & getPlayerChoice func

--- script.js
+++ script.js
@@ -1,10 +1,25 @@
+const choices = ["rock", "paper", "scissors"];
+console.log(getPlayerChoice());
+
 function getComputerChoice() {
 
     // 1. obtain random choice from rock paper scissors
+    let computerChoice = choices[Math.floor(Math.random() * choices.length)];
 
     // 2. return random choice as output from function
+    return computerChoice;
 };
 
+function getPlayerChoice() {
+    var choice = "hello";
+
+    while (choices.includes(choice) == false) {
+        console.log("Choice must be rock, paper or scissors");
+        var choice = prompt("Choose a hand to play").toLowerCase();
+    }
+    return choice
+
+};
 function playRound(playerChoice, computerChoice) {
     // 1. Compare player choice to computer choice
 
@@ -22,8 +37,11 @@ function playRound(playerChoice, computerChoice) {
 function game() {
     // set up for loop to play 5 rounds
         // obtain user input
+        let playerChoice = getPlayerChoice();
         // obtain computer input
+        let computerChoice = getComputerChoice;
         // call playround with both inputs
+        let winner = playRound(getPlayerChoice, getcomputerChoice);
         // update score
 
     // once loop is complete, return rounds won, lost and drawn.
